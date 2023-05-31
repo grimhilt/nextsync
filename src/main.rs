66ee1e91919326e4a5d5f87ddc9e06166cdf9be8
grimhilt <grimhilt@users.noreply.github.com>
2023-05-31 16:49:13 +0200

    upload file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use reqwest::Client;
+use std::fs::File;
+use std::io::Read;
 use reqwest::header::{HeaderValue, CONTENT_TYPE, HeaderMap};
 use std::error::Error;
 use std::env;
@@ -54,9 +56,44 @@ async fn send_propfind_request() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+async fn upload_file(path: &str) -> Result<(), Box<dyn Error>> { 
+    dotenv().ok();
+    
+    let mut api_endpoint = env::var("HOST").unwrap().to_owned();
+    api_endpoint.push_str("/remote.php/dav/files/");
+    let username = env::var("USERNAME").unwrap();
+    api_endpoint.push_str(&username);
+    api_endpoint.push_str("/test/ok");
+    let password = env::var("PASSWORD").unwrap();
+    
+    let mut file = File::open("./file.test")?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+
+    // Create a reqwest client
+    let client = Client::new();
+
+    // Send the request
+    let response = client
+        .request(reqwest::Method::PUT, api_endpoint)
+        .basic_auth(username, Some(password))
+        .body(buffer)
+        .send()
+        .await?;
+
+    // Handle the response
+    if response.status().is_success() {
+        let body = response.text().await?;
+        println!("Response body: {}", body);
+    } else {
+        println!("Request failed with status code: {}", response.status());
+    }
+    Ok(())
+}
+
 fn main() {
     tokio::runtime::Runtime::new().unwrap().block_on(async {
-        if let Err(err) = send_propfind_request().await {
+        if let Err(err) = upload_file("tkt").await {
             eprintln!("Error: {}", err);
         }
     });
