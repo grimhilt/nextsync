709f0f3b84c1f064f944a33fa07a8d009ddf5dfb
grimhilt <grimhilt@users.noreply.github.com>
2023-05-31 23:20:56 +0200

    done init

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,9 +1,20 @@
-use std::fs::DirBuilder;
+use std::fs::{DirBuilder, File};
+use std::env;
 
 pub fn init() {
     let builder = DirBuilder::new();
-    match builder.create("./test/dir") {
+    let mut path = env::current_dir().unwrap();
+    path.push(".nextsync");
+    match builder.create(path.clone()) {
         Ok(()) => println!("Directory successfuly created"),
         Err(_) => println!("Error: cannot create directory"),
+    }
+
+    path.pop();
+    path.push(".nextsyncignore");
+    
+    match File::create(path) {
+        Ok(_) => println!("File successfuly created"),
+        Err(_) => println!("Error: cannot create .nextsyncignore"),
     }
 }
